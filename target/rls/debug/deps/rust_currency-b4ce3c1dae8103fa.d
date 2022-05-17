664319a112a5e1d2c0c77d0e3e723e25da81a031
Ted1166 <ogolated00@gmail.com>
2022-05-17 20:05:57 +0300

    rust_currency

---
+++ target/rls/debug/deps/rust_currency-b4ce3c1dae8103fa.d
@@ -0,0 +1,5 @@
+c:\Users\User\Desktop\rust_currency\target\rls\debug\deps\rust_currency-b4ce3c1dae8103fa.rmeta: src\main.rs
+
+c:\Users\User\Desktop\rust_currency\target\rls\debug\deps\rust_currency-b4ce3c1dae8103fa.d: src\main.rs
+
+src\main.rs:
---
+++ target/rls/debug/deps/rust_currency-e554a19781e5a928.d
@@ -0,0 +1,5 @@
+c:\Users\User\Desktop\rust_currency\target\rls\debug\deps\rust_currency-e554a19781e5a928.rmeta: src\main.rs
+
+c:\Users\User\Desktop\rust_currency\target\rls\debug\deps\rust_currency-e554a19781e5a928.d: src\main.rs
+
+src\main.rs:
